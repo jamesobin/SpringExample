ee1f93b2d53936854595fd4a64f38e087882a4d9
jamesobin <jamesobin@naver.com>
2024-11-01 18:11:35 +0900

    [권오빈]  - 스프링 예제

---
+++ src/main/java/com/jamesobin/spring/ex/database/repository/UsedGoodsRepository.java
@@ -0,0 +1,6 @@
+package com.jamesobin.spring.ex.database.repository;
+
+// 데이터베이스 관련 처리
+public interface UsedGoodsRepository {
+
+}
---
+++ src/main/java/com/jamesobin/spring/ex/database/service/UsedGoodsService.java
@@ -0,0 +1,17 @@
+package com.jamesobin.spring.ex.database.service;
+
+import org.springframework.stereotype.Service;
+
+// 로직(business)을 담당한다
+// 데이터 수정 가공
+// 그외 복잡한 기능 수행
+@Service
+public class UsedGoodsService {
+
+	// 중고물품 게시글 모두 얻어 오기
+	public  getUsedGoodsList() {
+		// usedgoods 테이블 조회 결과 얻어오기
+		
+	}
+	
+}
